80031c4f339e2bc0fd888ce612d5381936f14fb0
Jose Moreira <jgmoreira@gmail.com>
2020-11-17 09:28:55 -0800

    Update copy_mysql_files.go

--- copy_mysql_files.go
+++ copy_mysql_files.go
@@ -16,19 +16,22 @@ func destinationServer() (u string, s string) {
 }
 
 func copyMyCNF(uAccount, mysqlS string) {
-	copyMyCNFCommand := ("sshpass -f \"/tmp/j\" scp /home/hch/mysql/my.cnf " + uAccount + "@" + mysqlS + ":/tmp")
-	fmt.Println(copyMyCNFCommand)
+	copyMyCNFCommand := ("sshpass -f \"/home/" + uAccount + "/j\" scp /home/hch/mysql/my.cnf " + uAccount + "@" + mysqlS + ":/tmp")
+	//fmt.Printf("copyMyCNFCommand: %s", copyMyCNFCommand)
 	_, err := exec.Command("bash", "-c", copyMyCNFCommand).Output()
 
 	if err != nil {
-		fmt.Println("\n ❌  Couldn't copy my.cnf into MySQL Server /tmp")
-		fmt.Println("\n Is sshpass installed? (execute \"sudo yum -y install sshpass\")")
+		fmt.Println("error  ! ")
+		fmt.Println(err)
+		/*	fmt.Println("\n ❌  Couldn't copy my.cnf into MySQL Server /tmp")
+			fmt.Println("\n ❌  Please check: ")
+			fmt.Println("\n ❌  1) Is sshpass installed? yum install -y sshpass")
+			fmt.Println("\n ❌  2) Have you ssh to the server before?")*/
 		panic(err)
 	}
 }
 func copyRMPs(uAccount, mysqlS string) {
-	copyRMPsCommand := ("sshpass -f \"/tmp/j\" scp /home/hch/mysql/*.rpm " + uAccount + "@" + mysqlS + ":/tmp")
-	//	fmt.Println(copyRMPsCommand)
+	copyRMPsCommand := ("sshpass -f \"/home/" + uAccount + "/j\" scp /home/hch/mysql/*.rpm " + uAccount + "@" + mysqlS + ":/tmp")
 	_, err := exec.Command("bash", "-c", copyRMPsCommand).Output()
 
 	if err != nil {
@@ -38,7 +41,7 @@ func copyRMPs(uAccount, mysqlS string) {
 }
 
 func copyClustercheck(uAccount, mysqlS string) {
-	copyClustercheckCommand := ("sshpass -f \"/tmp/j\" scp /home/hch/mysql/clustercheck* " + uAccount + "@" + mysqlS + ":/tmp")
+	copyClustercheckCommand := ("sshpass -f \"/home/" + uAccount + "/j\" scp /home/hch/mysql/clustercheck* " + uAccount + "@" + mysqlS + ":/tmp")
 	//	fmt.Println(copyClustercheckCommand)
 	_, err := exec.Command("bash", "-c", copyClustercheckCommand).Output()
 
@@ -48,7 +51,7 @@ func copyClustercheck(uAccount, mysqlS string) {
 	}
 }
 func copyNrpe(uAccount, mysqlS string) {
-	copyNrpeCommand := ("sshpass -f \"/tmp/j\" scp /home/hch/nrpe.service " + uAccount + "@" + mysqlS + ":/tmp")
+	copyNrpeCommand := ("sshpass -f \"/home/" + uAccount + "/j\" scp /home/hch/nrpe.service " + uAccount + "@" + mysqlS + ":/tmp")
 	//	fmt.Println(copyNrpeCommand)
 	_, err := exec.Command("bash", "-c", copyNrpeCommand).Output()
 
@@ -58,11 +61,11 @@ func copyNrpe(uAccount, mysqlS string) {
 	}
 }
 
-func createPWDFile() {
+func createPWDFile(acc string) {
 	var u string
-	f, err1 := os.Create("/tmp/j")
+	f, err1 := os.Create("/home/" + acc + "/j")
 	if err1 != nil {
-		fmt.Println("\n ❌  Could not create \"/tmp/j\" file! ")
+		fmt.Printf("\n ❌  Could not create \"/home/%s/j\" file! \n", acc)
 		panic(err1)
 	}
 	fmt.Printf("\nEnter your password for ssh: ")
@@ -75,7 +78,7 @@ func createPWDFile() {
 	// check if file exists
 	_, err3 := os.Stat("/tmp/j")
 	if os.IsNotExist(err3) {
-		fmt.Println("\n ❌  \"/tmp/j\" file does not exist! ")
+		fmt.Printf("\n ❌  \"/home/%s/j\"  file does not exist! ", acc)
 		panic(err3)
 	}
 }
@@ -85,7 +88,7 @@ func main() {
 	// destinationServer - takes the User Account and the MySQL Server as an input
 	userAccount, mysqlServer := destinationServer()
 	// createPWDFile - creates /tmp/j file with the user account password  (required for SSH). This file will be deleted at the end of the script
-	createPWDFile()
+	createPWDFile(userAccount)
 	// all the following copy the required files from the jumphost to the MySQL server
 	copyMyCNF(userAccount, mysqlServer)
 	copyRMPs(userAccount, mysqlServer)
@@ -93,8 +96,9 @@ func main() {
 	copyNrpe(userAccount, mysqlServer)
 
 	// deleting the /tmp/j containing the password file
-	e := os.Remove("/tmp/j")
+	e := os.Remove("/home/" + userAccount + "/j")
 	if e != nil {
+		fmt.Println()
 		log.Fatal(e)
 	}
 }
